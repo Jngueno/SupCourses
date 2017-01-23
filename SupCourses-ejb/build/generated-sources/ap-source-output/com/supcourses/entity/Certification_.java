42f04d9beb273ffdc9cc0dc6fe212d6050e5cd49
Jennyfer NGUENO <jngueno@yahoo.fr>
2017-01-23 16:45:56 +0100

    Premier commit

---
+++ SupCourses-ejb/build/generated-sources/ap-source-output/com/supcourses/entity/Certification_.java
@@ -0,0 +1,19 @@
+package com.supcourses.entity;
+
+import com.supcourses.entity.Course;
+import com.supcourses.entity.StudentCertification;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.CollectionAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-01-17T09:19:52")
+@StaticMetamodel(Certification.class)
+public class Certification_ { 
+
+    public static volatile SingularAttribute<Certification, Integer> score;
+    public static volatile SingularAttribute<Certification, String> certificationId;
+    public static volatile CollectionAttribute<Certification, StudentCertification> studentCertificationCollection;
+    public static volatile SingularAttribute<Certification, Course> courseId;
+
+}
---
+++ SupCourses-ejb/build/generated-sources/ap-source-output/com/supcourses/entity/Question_.java
@@ -0,0 +1,19 @@
+package com.supcourses.entity;
+
+import com.supcourses.entity.Quizz;
+import com.supcourses.entity.Response;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.CollectionAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-01-17T09:19:52")
+@StaticMetamodel(Question.class)
+public class Question_ { 
+
+    public static volatile SingularAttribute<Question, String> questionId;
+    public static volatile SingularAttribute<Question, String> questiionContent;
+    public static volatile SingularAttribute<Question, Quizz> quizzId;
+    public static volatile CollectionAttribute<Question, Response> responseCollection;
+
+}
